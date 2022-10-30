71085960a56ea67944723d954ca6cb354ee228ae
erizo47 <erizo4747@gmail.com>
2022-10-31 00:37:40 +0400

    arrows control

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 function slidesPlugin () {
     const slides = document.querySelectorAll('.slide');
-
     const colorsLight = [
         '#383447',
         '#333e3d',
@@ -30,28 +29,74 @@ function slidesPlugin () {
         '#2C2911',
         '#202C11'
     ]
-    let activeSlides = Math.floor(Math.random() * slides.length);
-    slides[activeSlides].classList.add('active')
+    let startActiveSlide = Math.floor(Math.random() * slides.length);
+    let currentSlide = 0
+    currentSlide = startActiveSlide
+    console.log(currentSlide)
+    slides[startActiveSlide].classList.add('active')
     
     for ( const slide of slides) {   
         const colorUp = Math.floor(Math.random() * colorsLight.length);
         const colorDown = Math.floor(Math.random() * colorsDark.length);
         const background = `linear-gradient(${colorsLight[colorUp]}, ${colorsDark[colorDown]})`
+
         function changeBackground() {    
             document.body.style.background = background
         }
+
         slide.addEventListener('click', () => {
             clearActiveClasses()
             slide.classList.add('active')
+            currentSlide = slide.id
+            console.log(currentSlide)
             changeBackground()
         })
+
     }
+
+    
+    document.addEventListener('keydown', (event => {
+        if (event.key === 'ArrowRight') {
+            console.log("right")
+            changeSlide('next')
+        } else if (event.key === 'ArrowLeft') {
+            console.log("left")
+            changeSlide('prev')
+        }
+    }))
+
     function clearActiveClasses() {
         slides.forEach((slide) => {
             slide.classList.remove('active')
         })
     }
-    
+
+    function changeSlide(direction) {
+        
+        if (direction === 'next') {
+            if (currentSlide === 5) {
+                clearActiveClasses()
+                currentSlide = 0
+                slides[currentSlide].classList.add('active')
+            } else {
+                clearActiveClasses()
+                currentSlide++
+                slides[currentSlide].classList.add('active')
+            }
+        } else if (direction === 'prev') {
+            if (currentSlide === 0) {
+                clearActiveClasses()
+                currentSlide = 5
+                slides[currentSlide].classList.add('active')
+            } else {
+                clearActiveClasses()
+                currentSlide--
+                slides[currentSlide].classList.add('active')
+            }
+        }
+    }
 }
   
 slidesPlugin ();
+
+
